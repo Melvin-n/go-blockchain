dc596f0d49edacb6b0d8030b46cd330b07e1faf0
melvin <melvin.narayan@9spokes.com>
2022-08-05 17:01:12 +1200

    changed createhash return value to string, added block generate function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"time"
 )
@@ -16,10 +17,20 @@ type Block struct {
 
 var BlockChain []Block
 
-func createHash(block Block) [32]byte {
+func createHash(block Block) string {
 	record := fmt.Sprintf("%d%s%d%s", block.Index, block.Timestamp.String(), block.BPM, block.PrevHash)
-
 	hashed := sha256.Sum256([]byte(record))
 
-	return hashed
+	//[:] turns array into slice, and slices have capabilities that arrays don't e.g converting to string
+	return hex.EncodeToString(hashed[:])
+}
+
+func generateNewBlock(timestamp time.Time, bpm int, prevHash string, prevBlock Block) {
+	var newBlock Block
+
+	newBlock.Index = prevBlock.Index + 1
+	newBlock.Timestamp = time.Now()
+	newBlock.BPM = bpm
+	newBlock.PrevHash = prevBlock.Hash
+	newBlock.Hash = createHash(newBlock)
 }
